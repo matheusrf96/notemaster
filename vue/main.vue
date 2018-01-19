7c13cdff9d417d73f9253e6f9e98705cb6549840
Matheus R <matheusrf96@gmail.com>
2018-01-19 20:18:11 -0200

    Primeiro commit

---
+++ vue/main.vue
@@ -0,0 +1,31 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        title: "Notemaster",
+        note: {
+            title: '',
+            content: ''
+        },
+        notes: [
+            {
+                title: "Test 01",
+                content: "Primeiro Text",
+                date: new Date(Date.now()).toLocaleString()
+            }
+        ]
+    },
+    methods: {
+        addNote(){
+            let {title, content} = this.note
+
+            this.notes.push({
+                title,
+                content,
+                date: new Date(Date.now()).toLocaleString()
+            })
+        },
+        removeNote(index){
+            this.notes.splice(index, 1)
+        }
+    }
+})
